3fa635d678dedf36e812335f1787100c160f8067
DOCENCIA <DOCENCIA@U026339.xa.bp.ehu.es>
2015-10-08 09:23:02 +0200

    Proba kasua "src" karpetan

---
+++ src/org/minakdev/aktoreak/test/PelikulaTest.java
@@ -0,0 +1,14 @@
+package org.minakdev.aktoreak.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PelikulaTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/org/minakdev/aktoreak/test/PelikulakTest.java
@@ -0,0 +1,14 @@
+package org.minakdev.aktoreak.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PelikulakTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
