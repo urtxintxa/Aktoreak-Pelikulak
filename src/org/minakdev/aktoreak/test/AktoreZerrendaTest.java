bfd4396af5e6f765d12f0ec8797b73313b797ae9
amaia <amitxu96@gmail.com>
2015-10-12 14:17:37 +0200

    AktoreZerrenda test

--- src/org/minakdev/aktoreak/test/AktoreZerrendaTest.java
+++ src/org/minakdev/aktoreak/test/AktoreZerrendaTest.java
@@ -13,6 +13,8 @@ public class AktoreZerrendaTest {
 	private AktoreZerrenda aktoreZerrenda;
 	private Aktorea aktore1;
 	private Aktorea aktore2;
+	private Aktorea aktore3;
+	private Aktorea aktore4;
 
 	@Before
 	public void setUp() throws Exception {
@@ -20,6 +22,8 @@ public class AktoreZerrendaTest {
 		
 		aktore1 = new Aktorea("Aktore1");
 		aktore2 = new Aktorea("Aktore2");
+		aktore3 = new Aktorea("Aktore3");
+		aktore4 = new Aktorea("Aktore4");
 	}
 
 	@After
@@ -27,6 +31,8 @@ public class AktoreZerrendaTest {
 		aktoreZerrenda = null;
 		aktore1 = null;
 		aktore2 = null;
+		aktore3 = null;
+		aktore4 = null;
 	}
 	
 	@Test
@@ -40,38 +46,117 @@ public class AktoreZerrendaTest {
 	
 	@Test
 	public void aktoreaTxertatu() {
-		aktoreZerrenda.aktoreaTxertatu("Aktore1");
+		
+		aktoreZerrenda.aktoreaTxertatu("Aaktore");
 		assertSame(aktoreZerrenda.getZerrenda().size(), 1);
 		
-		aktoreZerrenda.aktoreaTxertatu("Aktore1");
+		aktoreZerrenda.aktoreaTxertatu("Aaktore");
 		assertSame(aktoreZerrenda.getZerrenda().size(), 1);
 		
-		aktoreZerrenda.aktoreaTxertatu("Aktore2");
+		aktoreZerrenda.aktoreaTxertatu("Baktore");
 		assertSame(aktoreZerrenda.getZerrenda().size(), 2);
+		
+		aktoreZerrenda.aktoreaTxertatu("Baktore");
+		assertSame(aktoreZerrenda.getZerrenda().size(), 2);
+		
+		aktoreZerrenda.aktoreaSartu(new Aktorea("Caktore"));
+		
+		aktoreZerrenda.aktoreaTxertatu("Baktore");
+		assertSame(aktoreZerrenda.getZerrenda().size(), 3);
+		
+		aktoreZerrenda.aktoreaTxertatu("AAA");
+		assertSame(aktoreZerrenda.getZerrenda().size(), 4);
+		assertSame(aktoreZerrenda.getZerrenda().indexOf(aktoreZerrenda.aktoreaBilatu("AAA")), 0);
+		
+		aktoreZerrenda.aktoreaTxertatu("BBB");
+		assertSame(aktoreZerrenda.getZerrenda().size(), 5);
+		assertSame(aktoreZerrenda.getZerrenda().indexOf(aktoreZerrenda.aktoreaBilatu("BBB")), 2);
+		
+		aktoreZerrenda.aktoreaTxertatu("ZZZ");
+		assertSame(aktoreZerrenda.getZerrenda().size(), 6);
+		assertSame(aktoreZerrenda.getZerrenda().indexOf(aktoreZerrenda.aktoreaBilatu("ZZZ")), 5);
+		
+	
+		
 	}
 	
 	@Test
 	public void aktoreaBilatu() {
-		aktoreZerrenda.aktoreaSartu(aktore1);
-		
-		assertSame(aktoreZerrenda.aktoreaBilatu("Aktore1"), aktore1);
+		//zerrenda hutsa
 		assertSame(aktoreZerrenda.aktoreaBilatu("Aktore2"), null);
+		
+		//Zerrendan aktore bakarra egonik, hau egotea
+		aktoreZerrenda.aktoreaSartu(aktore1);
+		assertSame(aktoreZerrenda.aktoreaBilatu("Aktore1"), aktore1);
+		
+		//Zerrendan aktore bakarra egonik, hau ez egotea
+		assertSame(aktoreZerrenda.aktoreaBilatu("Aktore2"), null);
+		
+		
+		//Zerrendan aktore bat baino gehiago egonik:
+		aktoreZerrenda.aktoreaSartu(aktore2);
+		aktoreZerrenda.aktoreaSartu(aktore3);
+		aktoreZerrenda.aktoreaSartu(aktore4);
+		//Aktorea hasieran egotea
+		assertSame(aktoreZerrenda.aktoreaBilatu("Aktore1"), aktore1);
+		//Aktorea amaieran egotea
+		assertSame(aktoreZerrenda.aktoreaBilatu("Aktore4"), aktore4);
+		//Aktorea erdialdetik egotea
+		assertSame(aktoreZerrenda.aktoreaBilatu("Aktore2"), aktore2);
+		//Aktorea ez egotea
+		assertSame(aktoreZerrenda.aktoreaBilatu("Aktore5"), null);
 	}
 	
 	@Test
 	public void aktoreakOrdenatu() {
-		aktoreZerrenda.aktoreaSartu(aktore2);
-		aktoreZerrenda.aktoreaSartu(aktore1);
-		
-		assertSame(aktoreZerrenda.getZerrenda().get(0), aktore2);
-		
+		//Zerrendan aktorerik ez izatea
 		aktoreZerrenda.aktoreakOrdenatu();
 		
-		assertSame(aktoreZerrenda.getZerrenda().get(0), aktore1);
+		//Zerrendak elementu bakarra izatea
+		aktoreZerrenda.aktoreaSartu(aktore1);
+		aktoreZerrenda.aktoreakOrdenatu();
+		assertSame(aktoreZerrenda.getZerrenda().indexOf(aktore1), 0);
+		
+		//Zerrendan elementu bat baino gehiago egotea, eta hauek ordenatuta egotea (Atik Zra)
+		aktoreZerrenda=new AktoreZerrenda();
+		aktoreZerrenda.aktoreaSartu(aktore1);
+		aktoreZerrenda.aktoreaSartu(aktore2);
+		aktoreZerrenda.aktoreaSartu(aktore3);
+		aktoreZerrenda.aktoreaSartu(aktore4);
+		aktoreZerrenda.aktoreakOrdenatu();
+		assertSame(aktoreZerrenda.getZerrenda().indexOf(aktore1), 0);
+		assertSame(aktoreZerrenda.getZerrenda().indexOf(aktore2), 1);
+		assertSame(aktoreZerrenda.getZerrenda().indexOf(aktore3), 2);
+		assertSame(aktoreZerrenda.getZerrenda().indexOf(aktore4), 3);
+		
+		//Zerrendan elementu bat baino gehiago egotea, eta hauek ordenatuta egotea (Ztik Ara)
+		aktoreZerrenda=new AktoreZerrenda();
+		aktoreZerrenda.aktoreaSartu(aktore4);
+		aktoreZerrenda.aktoreaSartu(aktore3);
+		aktoreZerrenda.aktoreaSartu(aktore2);
+		aktoreZerrenda.aktoreaSartu(aktore1);
+		aktoreZerrenda.aktoreakOrdenatu();
+		assertSame(aktoreZerrenda.getZerrenda().indexOf(aktore1), 0);
+		assertSame(aktoreZerrenda.getZerrenda().indexOf(aktore2), 1);
+		assertSame(aktoreZerrenda.getZerrenda().indexOf(aktore3), 2);
+		assertSame(aktoreZerrenda.getZerrenda().indexOf(aktore4), 3);
+		
+		//Zerrendan elementu bat baino gehiago egotea, eta hauek desordenatuta egotea
+		aktoreZerrenda=new AktoreZerrenda();
+		aktoreZerrenda.aktoreaSartu(aktore4);
+		aktoreZerrenda.aktoreaSartu(aktore1);
+		aktoreZerrenda.aktoreaSartu(aktore2);
+		aktoreZerrenda.aktoreaSartu(aktore3);
+		aktoreZerrenda.aktoreakOrdenatu();
+		assertSame(aktoreZerrenda.getZerrenda().indexOf(aktore1), 0);
+		assertSame(aktoreZerrenda.getZerrenda().indexOf(aktore2), 1);
+		assertSame(aktoreZerrenda.getZerrenda().indexOf(aktore3), 2);
+		assertSame(aktoreZerrenda.getZerrenda().indexOf(aktore4), 3);
 	}
 	
 	@Test
 	public void aktoreaEzabatu() {
+		
 		aktoreZerrenda.aktoreaSartu(aktore1);
 		assertSame(aktoreZerrenda.getZerrenda().size(), 1);
 		
